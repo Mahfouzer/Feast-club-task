9a4336cc5e35ef815ec043d456193b6480dd66cb
NaderMahfouz <nader.mahfouz@beyond-solution.com>
2021-05-22 18:00:35 +0200

    adding default colors and fonts to help whith styling

---
+++ constants.ts
@@ -0,0 +1,13 @@
+const COLORS = {
+    WHITE: "white",
+    BLACK: "black",
+    GREEN: "green"
+}
+
+const FONT_SIZES = {
+    NORMAL: 16,
+    HEADING: 20,
+    SUBHEADING: 18
+}
+
+export { COLORS, FONT_SIZES }
