d30398e1eba080385fd4ea56df1c8e53856253d8
NaderMahfouz <nader.mahfouz@beyond-solution.com>
2021-05-24 17:56:10 +0200

    integrate react-query with the application

--- App.tsx
+++ App.tsx
@@ -11,21 +11,28 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { AppTheme } from './constants';
 
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+
+const queryClient = new QueryClient();
 const Stack = createStackNavigator();
-
-
 
 
 function App() {
   return (
-    <NavigationContainer theme={AppTheme}>
-      <SafeAreaView style={styles.AppContainer}>
-        <Stack.Navigator>
-          <Stack.Screen name="Movies" component={MoviesScreen} />
-          <Stack.Screen name="MovieDetails" component={MovieDetailsScreen} />
-        </Stack.Navigator>
-      </SafeAreaView>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer theme={AppTheme}>
+        <SafeAreaView style={styles.AppContainer}>
+          <Stack.Navigator>
+            <Stack.Screen name="Movies" component={MoviesScreen} />
+            <Stack.Screen name="MovieDetails" component={MovieDetailsScreen} />
+          </Stack.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
+    </QueryClientProvider>
 
   );
 }
